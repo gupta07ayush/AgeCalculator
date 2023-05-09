46fe8f17f45655587089c65e1552e81be27babc5
Ayush Gupta <gupta07ayush@gmail.com>
2023-05-09 19:47:54 +0530

    heading label

--- ageCalculator.py
+++ ageCalculator.py
@@ -1,5 +1,5 @@
 # Import some functions from tkinter module
-from tkinter import Entry, Button, Tk
+from tkinter import Entry, Button, Tk, Label
 
 # import messagebox  class from tkinter
 from tkinter import messagebox
@@ -15,6 +15,11 @@ root.geometry("700x400")
 
 # set the background color of whole GUI window root
 root.config(bg="#344e41")
+
+# Heading Age calculator
+heading = Label(root, text='Age Calculator', font=(
+    'Times new roman', 50, 'bold'), bg="#3a5a40")
+heading.place(x=20, y=20, width=660, height=100)
 
 
 # start the GUI

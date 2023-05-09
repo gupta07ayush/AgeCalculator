d4dcd1aa8da6c9b0d8e33ce4fd0925c68f243106
Ayush Gupta <gupta07ayush@gmail.com>
2023-05-09 19:36:31 +0530

    initialize the root window

---
+++ ageCalculator.py
@@ -0,0 +1,21 @@
+# Import some functions from tkinter module
+from tkinter import Entry, Button, Tk
+
+# import messagebox  class from tkinter
+from tkinter import messagebox
+
+# create a object of TK class to create a GUI window
+root = Tk()
+
+# set the name of tkinter GUI window
+root.title('Age Calculator')
+
+# set the size of root window
+root.geometry("700x400")
+
+# set the background color of whole GUI window root
+root.config(bg="#344e41")
+
+
+# start the GUI
+root.mainloop()
